337d8cde960a9eabdb37eb3713a14e642977feb5
bood43v <budaevgena@gmail.com>
2022-10-15 14:33:08 +0900

    git commit 1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+int main(){
+	float x;
+
+	std::cout << " Enter number ";
+	std::cin >> x;
+	std::cout << " sqrt x = " << sqrt(x);
+
+	std::cout << " Git commit 1 ";
+
+}
